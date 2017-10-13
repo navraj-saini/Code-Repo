e220cfa8c5a95d5eaad484dc8077846eafc4b2c9
navraj-saini <32732032+navraj-saini@users.noreply.github.com>
2017-10-13 11:16:37 +0530

    Update regular_exp.py

--- regular_exp.py
+++ regular_exp.py
@@ -3,3 +3,15 @@ import re
 usr_inpt=str(input("Enter String: "))
 srch_exp=re.findall("Inform.",usr_inpt)
 print(srch_exp)
+
+
+
+#############################################################
+import re
+
+usr_inpt=str(input("Enter String: "))
+srch_exp=re.findall("Inform",usr_inpt)
+
+for i in srch_exp:
+    print(i)
+#############################################################
