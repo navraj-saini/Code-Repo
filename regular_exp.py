2922485d1940aa8b8d6846340b3921fc4bab0000
navraj-saini <32732032+navraj-saini@users.noreply.github.com>
2017-10-13 19:12:49 +0530

    Update regular_exp.py

--- regular_exp.py
+++ regular_exp.py
@@ -15,3 +15,17 @@ srch_exp=re.findall("Inform",usr_inpt)
 for i in srch_exp:
     print(i)
 #############################################################
+
+Regular Expression
+
+import re
+
+strn=input("Enter String: ")
+
+if regex is re.findall("\w{0,6}.\w{0,6}@\w{0,6}.\w{0,3}",strn):
+    print("Valid Email")
+    print(regex)
+else:
+    print("Invalid")
+
+
