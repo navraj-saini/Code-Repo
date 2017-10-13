d6c33596c87fe9fbf16618fa1b9333282fa48e30
navraj-saini <32732032+navraj-saini@users.noreply.github.com>
2017-10-13 18:34:04 +0530

    Update Basic_Python.py

--- Basic_Python.py
+++ Basic_Python.py
@@ -5,3 +5,21 @@ print(l)
 p=[22,33]
 l.extend(p)
 print(l)
+
+
+
+
+#################################################
+
+#Shopping 
+
+list_x=[]
+
+Usr_inpt=int(input("Enter size of List: "))
+
+for i in range(1,Usr_inpt+1):
+    Shopping_Items=str(input("Enter {} Item : ".format(i)))
+    
+    list_x.append(Shopping_Items)
+    
+print(list_x
