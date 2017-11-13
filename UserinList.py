34ffd2e14373f7c796a698610c9b94c2bf30834e
navraj-saini <32732032+navraj-saini@users.noreply.github.com>
2017-11-13 11:23:29 +0530

    Update UserinList.py

--- UserinList.py
+++ UserinList.py
@@ -6,3 +6,21 @@ if name in Databse:
     print("Welcome back",name)
 else:
     print("Please Sign in first")
+    
+#########################################################################################  
+    
+Databse=["Harry","Vicky","Dami","Vijay"]
+
+name=str(input("Enter Name: "))
+
+if name in Databse:
+    print("Welcome back",name)
+else:
+    print("Please Sign in first")
+    new_user=str(input("Enter Name to Sign up: "))
+    x=Databse.append(new_user)
+    print("Successfull")
+    
+print(Databse)
+
+#########################################################################################
